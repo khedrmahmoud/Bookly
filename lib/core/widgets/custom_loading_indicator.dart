d88c80231e3ee37b488e80190fa1cef319ec538d
khedrmahmoud <khedrtfg@gmail.com>
2023-07-04 18:24:50 +0200

    add loading animation

--- lib/core/widgets/custom_loading_indicator.dart
+++ lib/core/widgets/custom_loading_indicator.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class CustomLoadingIndicar extends StatelessWidget {
   const CustomLoadingIndicar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: CircularProgressIndicator());
+    return Center(
+        child: LoadingAnimationWidget.dotsTriangle(
+            color: Colors.white.withOpacity(0.5), size: 40));
   }
 }
--- lib/features/home/presentation/views/widgets/custom_book_image.dart
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 
@@ -17,8 +18,10 @@ class CustomBookImage extends StatelessWidget {
               child: Shimmer.fromColors(
             baseColor: Colors.red,
             highlightColor: Colors.yellow,
-            child: Container(
-              height: 150,
+            child: LoadingAnimationWidget.twistingDots(
+              leftDotColor: const Color(0xFF1A1A3F),
+              rightDotColor: const Color(0xFFEA3799),
+              size: 30,
             ),
           )),
           fit: BoxFit.fill,
